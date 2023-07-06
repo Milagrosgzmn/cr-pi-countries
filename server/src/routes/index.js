22be4d6dd4f24325a98cda5815772aec60adba81
milagros <miliguzman0@gmail.com>
2023-07-06 00:25:20 -0300

    added routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,21 @@
 const { Router } = require("express");
 
+const getCountries = require('../handlers/countriesHandlers/getCountries');
+const getCountryById = require('../handlers/countriesHandlers/getCountryById');
+const getCountriesByName = require('../handlers/countriesHandlers/getCountriesByName');
+const getActivities = require('../handlers/activityHandlers/getActivities');
+const postActivity = require('../handlers/activityHandlers/postActivity');
+
 const router = Router();
+
+router.get('/countries', getCountries);
+router.get('/countries/name', getCountriesByName);
+router.get('/countries/:countryId', getCountryById);
+
+
+//router.get('/activities', getActivities);
+//router.post('/activities', postActivity);
+
+//router.delete('/activities/:id', deleteActivity);
 
 module.exports = router;
