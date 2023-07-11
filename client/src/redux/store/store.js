5d1684a30209f1abd1b06d73f3c6c631fdee25b7
milagros <miliguzman0@gmail.com>
2023-07-10 23:44:50 -0300

    created store

---
+++ client/src/redux/store/store.js
@@ -0,0 +1,6 @@
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from '../reducer/reducer.js';
+import thunk from 'redux-thunk';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+export default store;
