fb20ca0ed0499afdcca4e34cf292a69f1d9342fd
milagros <miliguzman0@gmail.com>
2023-07-17 17:59:02 -0300

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Home from './views/home/home';
+import Activities from './views/activities/activities';
+import Landing from './views/landing/landing';
+import Error from './views/error/error';
+import Detail from './views/details/detail';
+import NavBar from './components/NavBar/navBar';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from "react";
+import { getCountries, getActivities } from './redux/actions/actions';
+import { useDispatch } from 'react-redux';
+
 import './App.css'
+import FormPage from './views/forPage/formPage';
 
 function App() {
-  const [count, setCount] = useState(0)
+   const rutas = ['/home', '/activities', '/activities/new'];
+   const dispatch =  useDispatch();
+   let location = useLocation();
+
+   useEffect(()=>{
+      dispatch(getCountries());
+      dispatch(getActivities());
+  },[]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+         { (location.pathname.includes('detail') || rutas.includes(location.pathname)) && <NavBar></NavBar>}
+         
+         <Routes>
+            <Route path='/' element= {<Landing/>}/>
+            <Route path='/home' element= {<Home/>}/>
+            <Route path='/activities' element={<Activities/>} />
+            <Route path='/activities/new' element={<FormPage/>} />
+            <Route path='/detail/:id' element= {<Detail/>}/>
+            <Route path='*' element= {<Error/>}/>
+         </Routes>
+     </div>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+
+import App from './App.jsx';
+import { Provider } from 'react-redux';
+import store from './redux/store/store';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'))
+
+root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
